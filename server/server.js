ef26b59b6fd2781fce7ff0bfb37527a4797c88b4
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-11-02 23:29:34 +0700

    create server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const app = express()
+
+//connect to cloud mongodb
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: false,
+	})
+	.then(() => console.log("connect to mongodb successfully"))
+	.catch((err) => console.log(err))
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`start server in port ${port}`))
