8ea1cd7bbdaf33004624c660c07483fbc392b9c4
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-11-09 00:36:52 +0700

    add edit form

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -1,7 +1,35 @@
+import Navbar from "./Navbar"
+import styles from "./Edit.module.css"
+
 const Edit = () => {
-    return (
-        <div></div>
-    )
+	return (
+		<div>
+			<Navbar />
+			<form>
+				<h1>Edit task</h1>
+				<div className={styles.fromGroup}>
+					<label>Select day</label>
+					<input type="number" />
+				</div>
+				<div className={styles.fromGroup}>
+					<label>Select time</label>
+					<select>
+						<option>Morning</option>
+						<option>Afternoon</option>
+						<option>Evening</option>
+						<option>Night</option>
+					</select>
+				</div>
+				<div className={styles.fromGroup}>
+					<label>Task name</label>
+					<input type="text" />
+				</div>
+				<div className={styles.submit}>
+					<input type="Submit" value="submit" />
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default Edit
